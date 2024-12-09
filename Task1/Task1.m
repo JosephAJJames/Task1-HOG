4a121c4a2eeba96060b92b407da291aecf228164
JosephAJJames <130866326+JosephAJJames@users.noreply.github.com>
2024-12-09 21:08:28 +0000

    Update Task1.m

--- Task1/Task1.m
+++ Task1/Task1.m
@@ -1,16 +1,64 @@
-function extractedHOGFeatures = extractHOGFeatures (inputImage)
-% Your code here
+%Each block 
+
+
+
+
+function extractHOGFeatures(inputImage)
+
+    %function to calculate the gradient magnitude and angle of a 8x8 cell
+    function [magnitued, angle] = calculateGradientAndAngle(cell)
+
+        %defining the sobel filters
+        sobel_vert = [-1 -2 -1; 0 0 0; 1 2 1]; %filter to be used along y axis
+        sobel_horz = [-1 0 1; -2 0 2; -1 0 1]; %filter to be used along x axis
+
+        %applying filters to cell
+        gx = imfilter(cell, sobel_horz, "conv", "same");
+        gy = imfilter(cell, sobel_vert, "conv", "same");
+
+        %calculating the 
+        magnitued = sqrt(gx.^2 + gy.^2);
+        angle = rad2deg(atan2(gy, gx));
+
+        magnitued = magnitued / max(magnitued(:));
+
+        angle = mod(angle + 180, 180);
+    end
+
+
+
+
+    I_cell = [
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224;
+    0   32  64  96  128 160 192 224
+    ];
+
+
+    [magnitued, angle] = calculateGradientAndAngle(I_cell);
+
+
+
+    disp(magnitued);
+    disp(angle);
+
 end
 
 
 inputImage = imread("cameraman.tif");
+inputImage = im2double(inputImage);
 
 % Load the input image
 
 % Ensure the image is grayscale
-[rows, cols, colours] = size(inputImage)
-if colours > 1
-    inputImage = mean(inputImage, 3);
+[rows, cols, colours] = size(inputImage);
+if colours > 1 %if the image has more than 1 3rd dimenison(colour channel)
+    inputImage = mean(inputImage, 3); %take the mean of the 3rd dimension
 end
 
 
@@ -18,14 +66,14 @@ end
 new_rows = ceil(rows/8) * 8;
 new_cols = ceil(cols/8) * 8;
 
-if newHeight ~= height || newWidth ~= width
-    inputImage = imresize(inputImage, [newHeight, newWidth]);
+if new_rows ~= rows || new_cols ~= cols
+    inputImage = imresize(inputImage, [new_cols, new_rows]);
 end
 
 
 
 
 % Call the HOG feature extraction function
-%extractedHOGFeatures = extractHOGFeatures(inputImage);
+extractHOGFeatures(inputImage);
 % Display the length of the extracted features
 %disp(['Length of extracted HOG features: ',num2str(length(extractedHOGFeatures))]);
